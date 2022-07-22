57ee28c56b3bcad52734882e5eedaad0c3c6f5d3
minus-hunt <luchkintx@gmail.com>
2022-07-22 17:53:16 +0300

    Arguments in command line

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-import os
-from data import EXT, DIR_NAME, DIR_PATH
+''' Скрипт который переименовывает изображения в указанной дирректории с заданным корнем и порядковым номером. '''
+from os import listdir, rename
+from sys import argv
+
+# Параметры котрые мы передаем в программу в качестве аргументов
+script, path, dir_name = argv
 
 
 def get_files_list(path):
-    # Данная функция возвращает список файлов в котором содержатся нужные для переименования файлы
-    files = os.listdir(path)  # список файлов в текущей дирректории
+    '''
+    Данная функция возвращает список файлов в котором содержатся нужные для переименования файлы
+    :param path:
+    :return: files
+    '''
+    files = listdir(path)  # получение списка файлов в дирректории путь к которой передан в кач-ве параметра
 
     # Чистка списка
     for file in files:
-        if EXT in file:
+        if '.jpg' or '.JPG' or '.png' or '.PNG' or '.JPEG' or '.jpeg' in file:  # отбор файла с нужным расширением
             pass
         else:
             files.remove(file)
@@ -17,25 +25,38 @@ def get_files_list(path):
 
 
 def rename_files(files_list, path):
+    '''
+    Функия переименовывает файлы которые содержатся в поступающем списке
+    :param files_list:
+    :param path:
+    :return:
+    '''
     for file in files_list:
         file_old_name = path + file  # Старое имя файла вместе с путем
-        file_name = DIR_NAME + '_' + str(files_list.index(file)) + EXT  # Получаем новое имя файла
+        file_name = dir_name + '_' + str(files_list.index(file)) + '.jpg'  # Генерируем новое имя файла
         file_new_name = path + file_name  # Новое имя файла вместе с абсолютным путем
 
-        os.rename(file_old_name, file_new_name)
+        # Переименование файлов
+        rename(file_old_name, file_new_name)
 
 
 def main():
-    files_list = get_files_list(DIR_PATH)
-    print("[!] ОБРАТИТЕ ВНИМАНИЕ ЧТО ДЛЯ УСПЕШНОЙ РАБОТЫ СКРИПТА ПУТЬ ДОЛЖЕН ЗАКАНЧИВАТЬСЯ СЛЕШЕМ\n Желаете продолжить? (y/n)")
-    state = input()
+    files_list = get_files_list(path)
+    print(
+        "[!] ОБРАТИТЕ ВНИМАНИЕ ЧТО ДЛЯ УСПЕШНОЙ РАБОТЫ СКРИПТА ПУТЬ ДОЛЖЕН ЗАКАНЧИВАТЬСЯ СЛЕШЕМ\n Желаете продолжить? "
+        "(y/n)"
+    )
 
+    state = input().lower()
     if state == 'y':
-        rename_files(files_list, DIR_PATH)
+        rename_files(files_list, path)
     elif state == 'n':
+        print("Ну как хочешь...")
         quit()
     else:
-        print("Не понимаю о чем вы говорите")
+        print("Не понимаю о чем вы говорите. Бай!")
+        quit()
+    print('[Файлы в указанной дирректории переименованы.]')
 
 
 if __name__ == "__main__":
